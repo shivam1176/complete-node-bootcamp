6c4c36ab54e871208f725dd6e6188bd5d2bfe690
shivam1176 <jindalshivam009@gmail.com>
2024-06-26 11:22:17 +0530

    first commit
    first commit

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const fs= require('fs');
+const app = express();
+app.use(express.json());
+// app.get('/',(req,res)=>{
+//     // res.status(200).send('hello form the server ');
+//     res.status(200).json({message :'hello form the server ',
+//         app:'nature'
+//     });
+ 
+
+
+// })
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+) ;  
+app.get('/api/v1/tours',(req,res)=>{
+    res.status(200).json({
+      status:'success',
+      result:tours.length,
+      data:{
+        tours:tours
+      } 
+    })
+})
+app.post('/api/v1/tours',(req,res)=>{
+    // console.log(req.body);
+    const newId = tours[tours.length -1].id+1;
+    const newTour = Object.assign({id:newId},req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),
+    err=>{
+        res.status(201).json({
+            status:'success',
+            data :{
+                tour:newTour
+            }
+        })
+
+    })
+    // res.send('Done')
+
+})
+
+
+const port =3000;
+app.listen(port,()=>{
+    console.log(`app is running on ${port}...`);
+});
