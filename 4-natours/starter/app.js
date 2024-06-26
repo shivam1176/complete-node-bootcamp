2684116101c3d683127934de9fd69a9b5fb2cd36
shivam1176 <jindalshivam009@gmail.com>
2024-06-26 12:16:51 +0530

    added reapond to url parameters

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -14,14 +14,28 @@ app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 ) ;  
-app.get('/api/v1/tours',(req,res)=>{
+app.get('/api/v1/tours/:id',(req,res)=>{
+    console.log(req.param);
+    const id = req.params.id*1;
+    if(id>tours.length){
+        return res.status(404).json({
+            status:'fail',
+            message:'invalid ID'
+
+        });
+    }
+
+    const particularTour = tours.find(el=>el.id ===id)
+
     res.status(200).json({
       status:'success',
-      result:tours.length,
+      result:particularTour.length,
+    //   result:tours.length,
       data:{
-        tours:tours
+        tours:particularTour
+        // tours:tours
       } 
-    })
+    });
 })
 app.post('/api/v1/tours',(req,res)=>{
     // console.log(req.body);
@@ -43,7 +57,7 @@ app.post('/api/v1/tours',(req,res)=>{
 })
 
 
-const port =3000;
+const port =3001;
 app.listen(port,()=>{
     console.log(`app is running on ${port}...`);
 });
